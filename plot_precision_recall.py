3f5acc364f99fba996f82d694390448ba21a22d7
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2019-07-11 16:39:52 -0400

    plot figures based on object size

--- plot_precision_recall.py
+++ plot_precision_recall.py
@@ -10,43 +10,47 @@ import json
 import os
 import matplotlib.pyplot as plt
 
-folder_list={'yolov3_mobilenet':'YOLOv3 320x320',
-             'ssd_mobilenet_v2_224_bdd_autoratio':'Original SSD 224x224',
-             'ssd_mobilenet_v2_300_bdd_autoratio':'Original SSD 300x300',
-             's4cnn':'Our C4S [3]',
-             'ssd_mobilenet_opt_300_gt22':'Our SSD 300x300'
+folder_list={'ssd_mobilenet_v2_300_bdd':'Original SSD 300x300',
+             #'ssd_mobilenet_v2_224_bdd_autoratio':'SSD Auto Anchor Ratio 224x224',
+             'ssd_mobilenet_v2_300_bdd_autoratio':'SSD Auto Anchor Ratio 300x300',
+             #'s4cnn':'Our SSD with Our C4S [3]',
+             'ssd_mobilenet_opt_300':'Our SSD 300x300'
              }
-legendlist={'YOLOv3 320x320':[0.4745098, 0.9372549, 1.       ],
-            'Original SSD 224x224':[0.21176471, 0.85098039, 0.98039216],
-            'Original SSD 300x300':[0.12941176, 0.66666667, 1.        ],
-            'Our SSD 300x300':[1.        , 0.75294118, 0.        ],
-            'Our C4S [3]':[0.1254902 , 0.54117647, 0.99607843]
+legendlist={'Original SSD 300x300':[0.4745098, 0.9372549, 1.       ],
+            #'SSD Auto Anchor Ratio 224x224':[0.21176471, 0.85098039, 0.98039216],
+            'SSD Auto Anchor Ratio 300x300':[0.12941176, 0.66666667, 1.        ],
+            #'Our SSD with Our C4S [3]':[0.1254902 , 0.54117647, 0.99607843],
+            'Our SSD 300x300':[1.        , 0.75294118, 0.        ]
             }
-threshlist=[0.0001,
-            0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
-            0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95,
-            0.9999] #21 numbers in total
+threshlist=[0.0001, 0.01, 0.02, 0.03, 0.04, 
+                0.05, 0.1, 0.15, 0.2, 0.25, 
+                0.3, 0.35, 0.4, 0.45, 0.5, 
+                0.55, 0.6, 0.65, 0.7, 0.75, 
+                0.8, 0.85, 0.9, 0.95, 0.99] #25 numbers in total
 
 def plotPRcurve(performance, savepath=None):
     chartaxis = [0.0,1.0,0.0,1.0]
-    plt.figure(figsize=(8,6),dpi=100)
-    plt.axis(chartaxis)
     
-    for model in performance:
-        precision=[]
-        recall=[]
-        for conf in performance[model]:
-            precision.append(performance[model][conf]['overall']['precision'])
-            recall.append(performance[model][conf]['overall']['recall'])
-        plt.plot(recall,precision,marker='o',color=legendlist[model],label=model)
-    plt.title('Precision vs Recall of Our Models')
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.legend(loc=3)
-
-    if savepath is not None:
-        plt.savefig(os.path.join(savepath,'precision_vs_recall.png'))
-    plt.show()
+    categorylist=['overall','small','medium','large']
+    
+    for category in categorylist:
+        plt.figure(figsize=(8,6),dpi=100)
+        plt.axis(chartaxis)
+        for model in performance:
+            precision=[]
+            recall=[]
+            for conf in performance[model]:
+                precision.append(performance[model][conf][category]['precision'])
+                recall.append(performance[model][conf][category]['recall'])
+            plt.plot(recall,precision,marker='o',color=legendlist[model],label=model)
+        plt.title(category +' Precision vs Recall')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.legend(loc=3)
+    
+        if savepath is not None:
+            plt.savefig(os.path.join(savepath,category+'_precision_vs_recall.png'))
+        plt.show()
     
 def loadjson(loadpath,folderlist=folder_list):
     performance={}
