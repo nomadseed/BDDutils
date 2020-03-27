0740587f65eee30c830ad7467956892a6183d53a
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2020-03-26 21:29:02 -0400

    Add files via upload
    add some plotting functions

--- anchor_clustering.py
+++ anchor_clustering.py
@@ -148,7 +148,8 @@ def simpleKMeans(xlist,n_cluster=2,random_state=0,algorithm='auto'):
     
     return centroids, labels, score, processtime
 
-def adaptiveKMeans(bboxdict,max_num_center=10,scorethresh=0.8):
+def adaptiveKMeans(bboxdict,max_num_center=10,scorethresh=0.8,
+                   threshtype='unified'):
     """
     take in a bboxlist and perform k-means clustering where k ranges from 2 to
     a maximum (defalut is 10). 
@@ -162,13 +163,16 @@ def adaptiveKMeans(bboxdict,max_num_center=10,scorethresh=0.8):
     
     """
     clusterlist=[]
+    clusterlist_2=[]
     sumscore=0
-    for thresh in bboxdict:
+    groupdict={}
+    for l in bboxdict:
         grouplist=[]
         lastscore=float('Inf')
+        notsaved=True
         for n_cluster in range(2,max_num_center):
             # k-means clustering        
-            centroids, labels, score, processtime = simpleKMeans(xlist=bboxdict[thresh], n_cluster=n_cluster)
+            centroids, labels, score, processtime = simpleKMeans(xlist=bboxdict[l], n_cluster=n_cluster)
             print('clustering for k={}, processing time={}, score={}'.format(n_cluster,processtime,score))
             singledict={}
             singledict['k']=n_cluster
@@ -182,25 +186,101 @@ def adaptiveKMeans(bboxdict,max_num_center=10,scorethresh=0.8):
             sumscore+=score
             singledict['processtime']=processtime
             grouplist.append(singledict)
-            if score >= lastscore*scorethresh:
-                print('current score is {}% of last score, stop clustering for current layer'.format(score/lastscore))
-                break
+            print('current score is {}% of last score'.format(score/lastscore))
+            if notsaved and score/lastscore>=scorethresh:
+                clusterlist.append({'thresh':l, 'final_centro':singledict['centroids'],'totalscore':sumscore})        
+                notsaved=False
             lastscore=score
-        
-        clusterlist.append({'thresh':thresh, 'final_centro':singledict['centroids'],'totalscore':sumscore})
-    return clusterlist
+        groupdict[l]=grouplist
 
+    # return k values with two methods of applying threshed
+    
+    # first method is apply unified value for all the layers
+    if threshtype=='no_thresh':
+        # this branch is for using threshtype 'all', not 'mean' nor 'abs'
+        # which does not need effcost
+        return clusterlist, groupdict, []
+    
+    elif threshtype=='unified':
+        print('unified thresholds are applied')
+        scorelist=[i['totalscore'] for i in clusterlist]
+        layer=np.array([19*19,10*10,5*5,3*3,2*2,1*1])
+        centro=np.array([len(i['final_centro']) for i in clusterlist])
+        priors=np.sum(layer*centro)
+        centronum=sum(centro)
+        score=sum(scorelist)
+        totalentities=sum([len(bboxdict[i]) for i in bboxdict])
+        #avgscore=score/centronum
+        #print('avgscore: {}'.format(avgscore))
+        effcost=(score*priors)/(centronum*totalentities)
+        print('KMeans thresh={}'.format(scorethresh))
+        print('centro number: {}'.format(centronum))
+        print('priors: {}'.format(priors))
+        print('prior efficiency cost: {}'.format(effcost))
+        
+        return clusterlist, groupdict, []
+    
+    # second method is to find the combination of k values for the layers that
+    # creates the lowest effcost
+    elif threshtype=='argmin_effcost':
+        """
+        process the groupdict and minimize the effcose, return the k values
+        for each layer, k ranges from [2,6], hence there are 5^6=15625 
+        combinations to check. to reduce 
+        
+        """
+        print('argmin thresholds are applied')
+        layer=[19*19,10*10,5*5,3*3,2*2,1*1]
+        
+        sortresultlist=[]
+        for l in range(0,6):
+            #find the combination for the current layer that creates minimum effcost    
+            totalentities=len(bboxdict[l])
+            kmlist=groupdict[l]
+            centros=np.array([i['k'] for i in kmlist]) # all k values of current layer
+            scores=np.array([i['score'] for i in kmlist])
+            priors=layer[l]*centros
+            
+            
+            """
+            some works need to be done on this single formula
+            """
+            effcost=scores*priors
+            
+            # create keyword array to sort with effcost
+            dtype=[('k',int),('effcost',float)]
+            values=[(i,j) for i,j in zip(centros,effcost)]
+            kwdarr=np.array(values,dtype=dtype)
+            sortresult=np.sort(kwdarr,order='effcost')
+            sortresultlist.append(sortresult)
+            
+            opt_k, opt_score = sortresult[0]
+            print('for {}th layer, k={}, score={}'.format(l,opt_k,opt_score))
+            
+            #save the results to clusterlist No.2
+            for i in range(0,len(groupdict[l])):
+                if groupdict[l][i]['k']==opt_k:
+                    clusterlist_2.append({'thresh':l,
+                                'final_centro':groupdict[l][i]['centroids'],
+                                'totalscore':opt_score})
+                    break
+            
+        return clusterlist_2, groupdict, sortresultlist
+        
+    
+    
 def plotScatter(bboxdict, clusterdict, savepath='',samecolor=False):
     chartaxis = [0.0,310.0,0.0,310.0]
-    plt.figure(figsize=(6,6),dpi=100)
+    plt.figure(figsize=(7,6),dpi=100)
     plt.axis(chartaxis)
     if samecolor:
         colormap=['#0000ff','#0000ff']
     else:
-        colormap=['#0000ff', '#ffff00']# use html colors
+        colormap=['#0000ff', '#00ff00']# use html colors
     centrolist=[i['final_centro'] for i in clusterdict]
 
-    ax = plt.subplot(axisbg='black')
+    kwargs = {'family':'Times New Roman','fontsize':24}
+    ax = plt.subplot(axisbg='white')
     
     # plot scatter for benchmarks
     for i, thresh in zip(range(len(bboxdict)),bboxdict):
@@ -217,13 +297,13 @@ def plotScatter(bboxdict, clusterdict, savepath='',samecolor=False):
         area_anchor = np.ones(len(anchorlist))*100
         ax.scatter(x_anchor, y_anchor, s=area_anchor, alpha=1, c='r', marker='.')
     
-    ax.set_xlabel('Width')
-    ax.set_ylabel('Height')
-    plt.title('Distribution of Ground Truth & Default Boxes')
+    ax.set_xlabel('Width',**kwargs)
+    ax.set_ylabel('Height',**kwargs)
+    plt.title('Distribution of Ground Truth & Default Boxes',**kwargs)
     #plt.grid(True)
     plt.savefig(os.path.join(savepath,'scatter.png'),dpi=100)
     print('scatter saved as\n {}'.format(os.path.join(savepath,'scatter.png')))
-    plt.show()
+    #plt.show()
     #plt.close()
 
 def plotScatterSaved(bboxdict,clusterlist_1,clusterlist_2,
@@ -235,52 +315,58 @@ def plotScatterSaved(bboxdict,clusterlist_1,clusterlist_2,
     """
     
     chartaxis = [0.0,310.0,0.0,310.0]
-    plt.figure(figsize=(6,6),dpi=100)
+    plt.figure(figsize=(7,6),dpi=100)
     plt.axis(chartaxis)
+    kwargs = {'family':'Times New Roman','fontsize':24}
     
-    ax = plt.subplot(axisbg='black')
+    ax = plt.subplot(axisbg='white')
     
     # plot scatter for benchmarks
     for i, thresh in zip(range(len(bboxdict)),bboxdict):
         x_box = [i[0] for i in bboxdict[thresh]]
         y_box = [i[1] for i in bboxdict[thresh]]
         area_box = np.ones(len(x_box))*1
-        ax.scatter(x_box, y_box, s=area_box, alpha=0.2, c='b', marker='o')
+        ax.scatter(x_box, y_box, s=area_box, alpha=0.1, c='b', marker='o')
         
     # plot centroids of cluster result 1 in red
     for anchor in savedKmeanList[clusterlist_1]:
         x_anchor=anchor[1]
         y_anchor=anchor[0]
         ax.scatter(x_anchor, y_anchor, s=30, alpha=1, c='r', marker='o')
-    
+
     # plot centroids of cluster result 2 in blue
     for anchor in savedKmeanList[clusterlist_2]:
         x_anchor=anchor[0]
         y_anchor=anchor[1]
-        ax.scatter(x_anchor, y_anchor, s=30, alpha=1, c='y', marker='^')
-    
-    ax.set_xlabel('Width')
-    ax.set_ylabel('Height')
-    plt.title('Raw & Layer-wise K-means')
+        ax.scatter(x_anchor, y_anchor, s=30, alpha=1, c='g', marker='^')
+
+    ax.set_xlabel('Width',**kwargs)
+    ax.set_ylabel('Height',**kwargs)
+    plt.title('Raw & Layer-wise K-means',**kwargs)
     #plt.grid(True)
     plt.savefig(os.path.join(savepath,'kmeans_comparison.png'),dpi=100)
     print('scatter saved as\n {}'.format(os.path.join(savepath,'kmeans_comparison.png')))
     print('red dots are {}, yellow triangles are {}'.format(clusterlist_1,
                                          clusterlist_2))
-    plt.show()
+    #plt.show()
     #plt.close()
-    
-  
+
+PATH_DICT={'bdd':{'path':'D:/Private Manager/Personal File/uOttawa/Lab works/2018 fall/BerkleyDeepDrive/bdd100k',
+                  'label':'bdd100k_labels_images_train_VIVA_format_crop_gt22.json'},
+           'cal':{'path':'D:/Private Manager/Personal File/uOttawa/Lab works/2019 summer/caltech_dataset',
+                  'label':'caltech_annotation_VIVA_train.json'}
+           }
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', type=str, 
-                        default='D:/Private Manager/Personal File/uOttawa/Lab works/2018 fall/BerkleyDeepDrive/bdd100k', 
+                        default=PATH_DICT['cal']['path'], 
                         help="File path of input data")
     #D:/Private Manager/Personal File/uOttawa/Lab works/2018 fall/BerkleyDeepDrive/bdd100k
     #D:/Private Manager/Personal File/uOttawa/Lab works/2019 summer/caltech_dataset
     
     parser.add_argument('--json_label', type=str, 
-                        default='bdd100k_labels_images_train_VIVA_format_crop_gt22.json', 
+                        default=PATH_DICT['cal']['label'], 
                         help="label to specify json file")
     #bdd100k_labels_images_val_VIVA_format_crop.json
     #caltech_annotation_VIVA_test.json
@@ -303,9 +389,9 @@ if __name__=='__main__':
     
     # SETUPS
     #threshtype='abs'
-    threshtype='mean'
-    #threshtype='all'
-    scorethresh=0.8 # default=0.7
+    #threshtype='mean'
+    threshtype='all' # if use this, please change 'threshtype' in adaptiveKMeans to 'no_thresh'
+    scorethresh=0.7 # default=0.7
     #groupshape='belt'
     groupshape='sector'
     #groupshape='sickle'
@@ -337,34 +423,23 @@ if __name__=='__main__':
     #threshlist=[13.63, 17.64, 33.33, 75, 150, 500] # Threshlist for Caltech dataset, of grid size [22,17,9,4]
     elif threshtype=='all':
         threshlist=[[0,10000]]
-        scorethresh=1
+        scorethresh=0.7
     
     bboxdict = groupBBox(bboxlist,threshlist,groupshape=groupshape)
     
     # perform k-means for all the layers
     if not compareonly:
-        clusterdict = adaptiveKMeans(bboxdict,max_num_center=24,scorethresh=scorethresh)#max_num_center=25
+        clusterdict, groupdict, sortresultlist = adaptiveKMeans(bboxdict,
+                                                                max_num_center=10,
+                                                                scorethresh=scorethresh,
+                                                                threshtype='no_thresh')#max_num_center=25
         json.dump(clusterdict, open(os.path.join(savepath,'k-means_cluster_result_ssd.json'), 'w'),sort_keys=True, indent=4)
 
     # draw figures of [sum of error over k values]
     if not compareonly:
-        plotScatter(bboxdict, clusterdict, savepath=savepath,samecolor=False)
-        
-        # check IOU error after clustering
-        scorelist=[i['totalscore'] for i in clusterdict]
-        layer=np.array([19*19,10*10,5*5,3*3,2*2,1*1])
-        centro=np.array([len(i['final_centro']) for i in clusterdict])
-        priors=np.sum(layer*centro)
-        centronum=sum(centro)
-        score=sum(scorelist)
-        #avgscore=score/centronum
-        #print('avgscore: {}'.format(avgscore))
-        effcost=(score*priors)/(centronum*len(bboxlist))
+        plotScatter(bboxdict, clusterdict, savepath=savepath,samecolor=True)
         print('Layer-wise KMeans for {}-{} method'.format(groupshape,threshtype))
-        print('KMeans thresh={}'.format(scorethresh))
-        print('centro number: {}'.format(centronum))
-        print('priors: {}'.format(priors))
-        print('prior efficiency cost: {}'.format(effcost))
+        
         
     else:
         plotScatterSaved(bboxdict,
